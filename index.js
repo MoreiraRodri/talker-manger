90bebf7316130f7a12a2d5cc506b4c843bd3f1da
Rodrigo Moreira <rodrigossmoreira@hotmail.com>
2022-06-23 15:04:55 -0300

    retira de comentarios

--- index.js
+++ index.js
@@ -32,7 +32,6 @@ app.delete('/talker/:id', tokenValidation, deleteById);
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
-// não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
